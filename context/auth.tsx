63b35a0ca29333152fde85b31f1bdcda3f35ca7c
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-10-06 08:19:18 +0900

    authプロバイダ内の型定義

--- context/auth.tsx
+++ context/auth.tsx
@@ -1,6 +1,14 @@
-import React, { useContext } from 'react'
+import React, { ReactNode, useContext } from 'react'
 
-const AuthContext = React.createContext({
+interface AuthContextType {
+  login: (token: string) => void
+  logout: () => void
+}
+interface AuthProviderProps {
+  children: ReactNode
+}
+
+const AuthContext = React.createContext<AuthContextType>({
   login: () => {},
   logout: () => {},
 })
@@ -9,7 +17,7 @@ export const useAuth = () => {
   return useContext(AuthContext)
 }
 
-export const AuthProvider = ({ children }) => {
+export const AuthProvider = ({ children }: AuthProviderProps) => {
   const login = async (token: string) => {
     localStorage.setItem('auth_token', token)
   }
