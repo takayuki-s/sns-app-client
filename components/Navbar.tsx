ec49ce9a4e6f855547308c3c18ac90e46c9c768d
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-10-19 06:54:26 +0900

    ナビゲーションバー表示分け

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link'
 import React from 'react'
+import { useAuth } from '../context/auth'
 
 const Navbar = () => {
+  const { user, login } = useAuth()
   return (
     <header className="bg-gray-700 p-4 text-white">
       <div className="container mx-auto flex justify-between items-center">
@@ -12,20 +14,34 @@ const Navbar = () => {
         </h1>
         <nav>
           <ul className="flex space-x-4">
-            <>
-              <Link
-                href="/login"
-                className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
-              >
-                ログイン
-              </Link>
-              <Link
-                href="/signup"
-                className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
-              >
-                サインアップ
-              </Link>
-            </>
+            {user ? (
+              <>
+                <Link
+                  href={`/profile/${user.id}`}
+                  className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+                >
+                  プロフィール
+                </Link>
+                <button className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium">
+                  ログアウト
+                </button>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/login"
+                  className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+                >
+                  ログイン
+                </Link>
+                <Link
+                  href="/signup"
+                  className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+                >
+                  サインアップ
+                </Link>
+              </>
+            )}
           </ul>
         </nav>
       </div>
