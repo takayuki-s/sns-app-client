719b37ffde3e764daeb846a745a9ebcc630cabc6
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-10-24 06:51:33 +0900

    プロフィールAPIアクセス

--- pages/profile/[userId].tsx
+++ pages/profile/[userId].tsx
@@ -1,15 +1,45 @@
+import { GetServerSideProps } from 'next'
 import React from 'react'
+import apiClient from '../../lib/apiClient'
+import { Profile } from '../../types'
 
-const UserProfile = () => {
+type Props = {
+  profile: Profile
+}
+
+export const getServerSideProps: GetServerSideProps = async (context: any) => {
+  const { userId } = context.query
+  try {
+    const profileResponse = await apiClient.get(`/users/profile/${userId}`)
+    return {
+      props: {
+        profile: profileResponse.data,
+      },
+    }
+  } catch (err) {
+    console.log(err)
+    return {
+      notFound: true,
+    }
+  }
+}
+
+const UserProfile = ({ profile }: Props) => {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="w-full max-w-xl mx-auto">
         <div className="bg-white shadow-md rounded-lg p-6 mb-4">
           <div className="flex items-center">
-            <img className="w-20 h-20 rounded-full mr-4" alt="User Avatar" />
+            <img
+              className="w-20 h-20 rounded-full mr-4"
+              alt="User Avatar"
+              src={profile.profileImageUrl}
+            />
             <div>
-              <h2 className="text-2xl font-semibold mb-1">teke code</h2>
-              <p className="text-gray-600">はじめまして</p>
+              <h2 className="text-2xl font-semibold mb-1">
+                {profile.user.username}
+              </h2>
+              <p className="text-gray-600">{profile.bio}</p>
             </div>
           </div>
         </div>
